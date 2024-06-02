d35f1cf9e2d08d3f61e24114051a6267245b55ac
Fucci Im In <kimou998@gmail.com>
2024-06-01 17:04:51 -0700

    testing

--- src/components/LiveStream.js
+++ src/components/LiveStream.js
@@ -7,14 +7,14 @@ const LiveStream = () => {
       <h2>Live Stream</h2>
       <p>Watch the matches live here.</p>
       <iframe
-        width="560"
-        height="315"
-        src="https://www.youtube.com/embed/live_stream?channel=CHANNEL_ID"
-        title="Live Stream"
-        frameBorder="0"
-        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-        allowFullScreen
-      ></iframe>
+  width="560"
+  height="315"
+  src="https://www.youtube.com/live/jfKfPfyJRdk?si=Hd72lTrc227MUR3o"
+  title="Live Stream"
+  frameborder="0"
+  allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+  allowfullscreen
+></iframe>
     </div>
   );
 };
